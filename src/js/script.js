a4923b5846fe41faebf9093dc3944ac222045a73
Ксения <ksenya.zaharova92@gmail.com>
2020-06-20 22:39:46 +0300

    поправила адаптивную верстку

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,22 @@
-(function () {
+(function scroll() {
     const header = document.getElementById("mainNav");
-    window.onscroll = () => {
-        if (window.pageYOffset > 50) {
-            header.classList.add('mainNav_active');
-        } else {
-            header.classList.remove('mainNav_active');
+   // const value = window.innerWidth;
+
+        window.onscroll = () => {
+            if (window.pageYOffset > 50) {
+                header.classList.add('mainNav_active');
+            } else {
+                header.classList.remove('mainNav_active');
+            }
         }
-    }
+
+
 }());
+
+
+const btn = document.getElementById("navbar__btn");
+const navbar = document.getElementById("navbarResponsive");
+btn.addEventListener('click', () => {
+    btn.classList.toggle('collapsed')
+    navbar.classList.toggle('show')
+});
