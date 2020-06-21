9dee32b7380e9ca4b9fb8117e07b91b00942100e
Ксения <ksenya.zaharova92@gmail.com>
2020-06-21 18:37:59 +0300

    обновила версию сайта

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-(function () {
-    const header = document.getElementById("mainNav");
+
+const header = document.getElementById("mainNav");
+const btn = document.getElementById("navbar__btn");
+const navbar = document.getElementById("navbarResponsive");
+
+(function scroll() {
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             header.classList.add('mainNav_active');
@@ -8,3 +12,49 @@
         }
     }
 }());
+
+(function () {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('collapsed');
+        navbar.classList.toggle('show')
+    });
+})();
+
+(function () {
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight = document.querySelector('.navbar-brand').clientHeight + 40;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+
+        const ease = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+        navbar.classList.remove('show');
+        const animation = function (currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0, run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+
+    scrollTo();
+}());
+
